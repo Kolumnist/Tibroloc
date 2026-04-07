0831ef64b50e9caab55d8c7edbd7c41b1fd82e3e
Kolumnist <DarkSoul7@gmx.de>
2026-04-07 10:33:26 +0200

    adds table for viewing the data and asks for json file before start

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 const MAIN_BUTTON_NAMES = ["RESUME", "PAUSE"];
 
+var _startDiv = document.getElementById("startDiv");
+var _mainDiv = document.getElementById("mainDiv");
 var _mainButton = document.getElementsByName("mainButton")[0];
 var _time = document.getElementById("time");
 
+var _tableBody = document.getElementById("tableBody");
+var _fullIncome = document.getElementById("fullIncome");
+
 let intervalId = null;
+let fileHandle = null;
 
 let startTimeInSeconds = 0;
 let stopTimeInSeconds = 0;
 let currentTimeInSeconds = 0;
+let startDate = null;
 
-let startDate = new Date().toLocaleDateString('de-DE', {
-        day: '2-digit',
-        month: '2-digit',
-        year: '2-digit'
-});
 let incomePerHour = 18;
 
-startTimeInSeconds = parseInt(new Date().getTime() / 1000);
-intervalId ??= setInterval(setTime, 500);
+async function getJsonFile() {
+    [fileHandle] = await window.showOpenFilePicker();
+    let file = await fileHandle.getFile();
+    let content = await file.text();
+    
+    const data = JSON.parse(content);
+    //console.log(data[0].startDate);
+    return data;
+}
+
+async function onclickInit() {
+    try {
+        const data = await getJsonFile();
+        let fullIncome = 0;
+        let tableBodyContentString = "";
+        for (let i = 0; i < data.length; i++) {
+            fullIncome += data[i].income;
+            tableBodyContentString = tableBodyContentString.concat(
+                `<tr>
+                    <td>${data[i].startDate}</td>
+                    <td>${data[i].endDate}</td>
+                    <td>${data[i].time}</td>
+                    <td>${data[i].income}</td>
+                </tr>`
+            ); // MAYBE make the time only show hours and minutes cause that seems more plausible
+        }
+        _fullIncome.innerText = fullIncome+"\u20ac";
+        _tableBody.innerHTML = tableBodyContentString;
+
+        _startDiv.hidden = true;
+        _mainDiv.hidden = false;
+
+        startDate = new Date().toLocaleDateString('de-DE', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit'
+        });
+        startTimeInSeconds = parseInt(new Date().getTime() / 1000);
+        intervalId ??= setInterval(setTime, 500);
+    } catch (e) {
+        console.warn("User cancel or file error", e);
+    }
+}
 
 function formatTime() {
     let hours = parseInt(currentTimeInSeconds / 3600).toString().padStart(2, '0');
@@ -55,16 +98,18 @@ async function onclickSave() {
     let timeString = formatTime();
     let income = parseInt(incomePerHour * currentTimeInSeconds / 3600);
 
-    let [fileHandle] = await window.showOpenFilePicker();
-    let file = await fileHandle.getFile();
-    let content = await file.text();
-    
-    let jsonObject = JSON.parse(content);
-    jsonObject.push({startDate: startDate, endDate: endDateString, time: timeString, income: income});
+    try {
+        let file = await fileHandle.getFile();
+        let content = await file.text();
+        let jsonObject = JSON.parse(content);
+        jsonObject.push({startDate: startDate, endDate: endDateString, time: timeString, income: income});
 
-    let writable = await fileHandle.createWritable();
-    await writable.write(JSON.stringify(jsonObject, null, 2))
-    await writable.close().then(() => {
-        window.location.reload();
-    });
+        let writable = await fileHandle.createWritable();
+        await writable.write(JSON.stringify(jsonObject, null, 2))
+        await writable.close().then(() => {
+            window.location.reload();
+        });
+    } catch (e) {
+        console.error("Cannot find file", e);
+    }
 }
